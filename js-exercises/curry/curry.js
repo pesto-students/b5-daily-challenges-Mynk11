3d2ae80aff7e0f70b116aa4b3f1cf850afd2ecf6
Mynk11 <mayank@zonkafeedback.com>
2021-01-20 00:00:53 +0530

    curry function

--- js-exercises/curry/curry.js
+++ js-exercises/curry/curry.js
@@ -1,18 +1,10 @@
 function curry(func) {
   return (...args) => {
-    if (func.length === 1) {
-      return func(args[0]);
-    }
-    if (func.length === 2) {
-      return b => func(args[0], b);
-    }
-    if (func.length === 3 && args.length === 2) {
+    if (func.length === 1) return func(args[0]);
+    if (func.length === 2) return b => func(args[0], b);
+    if (func.length === 3 && args.length === 2)
       return b => func(args[0], args[1], b);
-    }
-
-    if (func.length === 3) {
-      return b => c => func(args[0], b, c);
-    }
+    if (func.length === 3) return b => c => func(args[0], b, c);
     if (func.length === 4) return b => c => d => func(args[0], b, c, d);
   };
 }
